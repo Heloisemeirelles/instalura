579ba8feb604549e0b0068a260ad195b99e01044
Heloise <heloisemeirelles@gmail.com>
2021-02-12 19:30:04 -0300

    Adicionando

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+//const Title = styled.h1`
+//  font-size: 50px;
+//..  color: ${({ theme }) => theme.colors.primary};
+//`
+function Title() {
+  return <h1>Titulo qualquer</h1>
+}
+
+export default function Home() {
+  return <div><Title />My page</div>
+}
